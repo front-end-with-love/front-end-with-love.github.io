69224d1f1c719106bf636639f6def6bd355167ff
Michail Khlebnikov <frontend.dev.ru@gmail.com>
2019-04-09 16:34:11 +0300

    Fixes

--- portfolio/test4/js/script.js
+++ portfolio/test4/js/script.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-  $("video").on("ended", function() {
-    TweenMax.to("form", 1, { delay: 1, opacity: 1 });
-    TweenMax.to("span", 1, {
-      delay: 0.5,
-      opacity: 1,
-      transform: "scale3d(1,1,1)"
-    });
-    TweenMax.to("h2", 1.5, { opacity: 1, transform: "scale3d(1,1,1)" });
-    TweenMax.to("#bottom_logo", 0.5, {
-      delay: 1.6,
-      visibility: "visible",
-      opacity: 1
-    });
-  });
+  var video = document.getElementById("video");
+  video.onended = function() {
+    setTimeout(function () {
+        $('form').addClass('active');
+    }, 1500);
+    setTimeout(function () {
+        $('span').addClass('active');
+    }, 1000);
+    setTimeout(function () {
+        $('h2').addClass('active');
+    }, 500);
+    setTimeout(function () {
+        $('#bottom_logo').addClass('active');
+    }, 1600);
+  };
 });
 
 $(document).ready(function() {
