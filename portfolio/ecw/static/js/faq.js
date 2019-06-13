ab2da0857de91fafa43abd4cdc66ae6b66168136
Michail Khlebnikov <frontend.dev.ru@gmail.com>
2019-06-13 10:51:39 +0300

    Fixes

--- portfolio/ecw/static/js/faq.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // hide/show faq item
-    $('.faq__item-title').on('click', function () {
-        $(this).closest('.faq__item').toggleClass('faq__item_active');
-    });
-});
