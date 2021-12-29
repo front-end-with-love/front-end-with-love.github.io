c12f221d370b63586c2df7aa33cfd3e4b4c11569
Michail Khlebnikov <frontend.dev.ru@gmail.com>
2021-12-29 20:59:18 +0300

    Fixes

--- portfolio/mms-layout/js/app-project-run.js
+++ portfolio/mms-layout/js/app-project-run.js
@@ -48,24 +48,6 @@
     });
   }
 
-  // Анимация появления элементов в блоке "Технологии"
-  function animateElementsInTechnology() {
-    $(window).scroll(function () {
-      let $curElement = $('.technology');
-      if ($curElement.length) {
-        let top_of_element = $curElement.offset().top;
-        let bottom_of_element = $curElement.offset().top + $curElement.outerHeight();
-        let bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-        let top_of_screen = $(window).scrollTop();
-
-        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-          $curElement.addClass('technology--animate');
-        } else {
-        }
-      }
-    });
-  }
-
   // Слайдер в карточках блока "Технологии"
   function initSliderTechnology(element, speed) {
     let $slider = $(element).find('.technology__item-slider');
@@ -82,7 +64,7 @@
   }
 
   // Открыть/закрыть мобильную навигацию
-  function showHideMobileNav () {
+  function showHideMobileNav() {
     $('.header__btn.header__btn--mobile').on('click', function () {
       $('.header__nav--mobile').toggleClass('header__nav--active');
       $('.header__btn.header__btn--mobile').toggleClass('header__btn--close');
@@ -95,57 +77,98 @@
   }
 
   // Продукты. Слайдер на мобильных устройствах : начало
-  function initProductsSlider () {
+  function initProductsSlider() {
+    // При загрузке страницы
     $(window).on('load', function () {
-      if ($(window).outerWidth() <= 1024) {
-        initSlick();
+      // Если область просмотра браузера больше 640 и меньше или равно 1024
+      if (640 < $(window).outerWidth() && $(window).outerWidth() <= 1024) {
+        initSlickPrimary();
       } else {
-        $('.products__container--list').filter('.slick-initialized').slick('unslick');
+        $('.products__container--primary-slider').filter('.slick-initialized').slick('unslick');
+      }
+
+      // Если область просмотра браузера меньше или равно 1024
+      if ($(window).outerWidth() <= 1024) {
+        initSlickSecondary();
+      } else {
+        $('.products__container--secondary-slider').filter('.slick-initialized').slick('unslick');
       }
     });
 
+    // При изменении размера окна
     $(window).on('resize', function () {
-      if ($(window).outerWidth() <= 1024) {
-        initSlick();
+      // Если область просмотра браузера больше 640 и меньше или равно 1024
+      if (640 < $(window).outerWidth() && $(window).outerWidth() <= 1024) {
+        initSlickPrimary();
       } else {
-        $('.products__container--list').filter('.slick-initialized').slick('unslick');
+        $('.products__container--primary-slider').filter('.slick-initialized').slick('unslick');
+      }
+
+      // Если область просмотра браузера меньше или равно 1024
+      if ($(window).outerWidth() <= 1024) {
+        initSlickSecondary();
+      } else {
+        $('.products__container--secondary-slider').filter('.slick-initialized').slick('unslick');
       }
     });
 
-    function initSlick() {
-      $('.products__container--list').slick({
+    function initSlickPrimary() {
+      $('.products__container--primary-slider').slick({
         centerMode: true,
         centerPadding: '40px',
         slidesToShow: 3,
         arrows: false,
+        dots: false,
         responsive: [
           {
             breakpoint: 768,
             settings: {
-              arrows: false,
               centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
+              centerPadding: '20px',
+              slidesToShow: 2,
+              arrows: false,
+              dots: false
             }
           },
           {
             breakpoint: 640,
+            settings: 'unslick'
+          }
+        ]
+      });
+    }
+
+    function initSlickSecondary() {
+      $('.products__container--secondary-slider').slick({
+        slidesToShow: 3,
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        prevArrow: $('.products__arrow--left'),
+        nextArrow: $('.products__arrow--right'),
+        responsive: [
+          {
+            breakpoint: 768,
             settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '30px',
-              slidesToShow: 1
+              slidesToShow: 1,
+              centerMode: false,
+              centerPadding: '0',
+              dots: false,
+              arrows: true,
+              prevArrow: $('.products__arrow--left'),
+              nextArrow: $('.products__arrow--right'),
             }
           }
         ]
       });
     }
   }
+
   // Продукты. Слайдер на мобильных устройствах : конец
 
   // Init
   initNav('.header__item--animate');
-  animateElementsInTechnology();
   initSliderTechnology('.technology__item--with-slider[data-slider="1"]', 4900);
   initSliderTechnology('.technology__item--with-slider[data-slider="2"]', 5800);
   initSliderTechnology('.technology__item--with-slider[data-slider="3"]', 6300);
